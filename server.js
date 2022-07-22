df04c1d1c6c2908bcac46344d87362c7934d5193
Dandd6541 <Mordkid4@gmail.com>
2022-07-22 14:12:00 -0700

    step 14

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var session = require('express-session');
+var passport = require('passport');
 const methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+
+require('./config/database');
+require('./config/passport');
+
 
 
 var indexRouter = require('./routes/index');
@@ -22,8 +29,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
